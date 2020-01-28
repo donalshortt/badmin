2bf6edfc2e4359e3032b8566a420deb4d2a13006
BuildTools <donalshortt@donals.eu>
2020-01-29 00:45:22 +0100

    worked on slime command

--- src/main/java/eu/donals/badmin/CommandSlime.java
+++ src/main/java/eu/donals/badmin/CommandSlime.java
@@ -1,6 +1,8 @@
 package eu.donals.badmin;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,6 +22,13 @@ public class CommandSlime implements CommandExecutor {
             Location location = player.getLocation();
 
             helper.spawnEntitysAroundCommander(player.getWorld(), location, EntityType.SLIME);
+
+            for (int x = -3; x < 4; x++){
+                for (int z = -3; z < 4; z++){
+                    player.getWorld().getBlockAt(player.getLocation().add(x, -1 , z)).setType(Material.SLIME_BLOCK);
+                }
+            }
+
         }
 
         return true;
